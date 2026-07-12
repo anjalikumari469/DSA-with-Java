c41ce8b8000d897a06eb1a8f0acd7968dca71d98
anjalikumari469 <kumarianjali28052007@gmail.com>
2026-07-12 16:26:34 +0530

    bitmanipulation commit

---
+++ Bit Manipulation/getbit.java
@@ -0,0 +1,12 @@
+public class getbit {
+    public static void main(String[] args) {
+        int n = 5; // 0101
+        int pos = 3;
+        int bitMask = 1 << pos;
+        if ((bitMask & n) == 0) {
+            System.out.println("Bit was zero");
+        } else {
+            System.out.println("Bit was one");
+        }
+    }
+}
---
+++ Bit Manipulation/updatebit.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class updatebit {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int oper = obj.nextInt();
+        // oper=1: set oper=0 : clear
+        int n = 5; // 0101 -> 0111 -> decimal 7
+        int pos = 1;
+
+        int bitMask = 1 << pos;
+        if (oper == 1) {
+            // set
+            int newNumber = bitMask | n;
+            System.out.println(newNumber);
+        } else {
+            // clear
+            int newBitMask = ~(bitMask);
+            int newNumber = newBitMask & n;
+            System.out.println(newNumber);
+        }
+    }
+}
