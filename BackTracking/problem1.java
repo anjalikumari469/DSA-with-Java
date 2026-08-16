06ea1325f8cbae140101c927c3643a5d51594740
anjalikumari469 <kumarianjali28052007@gmail.com>
2026-08-16 21:17:27 +0530

    bactracking commit

---
+++ BackTracking/problem1.java
@@ -0,0 +1,18 @@
+public class problem1 {
+    public static void printPermutation(String str, String perm, int indx) {
+        if (str.length() == 0) {
+            System.out.println(perm);
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char currChar = str.charAt(i);
+            String newstr = str.substring(0, i) + str.substring(i + 1);
+            printPermutation(newstr, perm + currChar, indx + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "ABC";
+        printPermutation(str, " ", 0);
+    }
+}
